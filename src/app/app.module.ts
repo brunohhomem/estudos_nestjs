9c3ce631cef8d00830859c274b565caa6b449e01
Bruno H. Homem <brunohhomem@gmail.com>
2025-11-13 10:25:11 -0300

    chore: add `conceitos-cli.module.ts` using nestjs cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
+import { ConceitosCliModule } from 'src/conceitos-cli/conceitos-cli.module';
 
 @Module({
-  imports: [ConceitosManualModule],
+  imports: [ConceitosManualModule, ConceitosCliModule],
   controllers: [AppController],
   providers: [AppService],
 })
