b806b370ddc4170cb2bc9d4b7f6353ea326c7371
Bruno H. Homem <brunohhomem@gmail.com>
2025-11-14 06:47:06 -0300

    remove: conceitos-manual and conceitos-cli

--- src/conceitos-cli/conceitos-cli.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ConceitosCliService {
-  solucionaCli(): string {
-    return 'home do conceitos cli';
-  }
-}
--- src/conceitos-manual/conceitos-manual.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ConceitosManualService {
-  solucionaHome(): string {
-    return 'home do conceitos manual'
-  }
-}
